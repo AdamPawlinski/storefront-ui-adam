dd558c7c34e2b2278ee2ab8c065c7b9a49b648a3
Adam Pawlinski <apawlinski@divante.co>
2019-08-28 16:36:27 +0200

    tests added

--- packages/vue/scripts/component-template/component.spec.ts
+++ packages/vue/scripts/component-template/component.spec.ts
@@ -1,18 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import SfNotification from "@/components/molecules/SfNotification/SfNotification.vue";
+import ComponentNameCamelCase from "@/components/ComponentFolder/ComponentNameCamelCase.vue";
 
-describe("SfNotification.vue", () => {
-  it("renders the notification", () => {
-    const component = shallowMount(SfNotification);
-    expect(component.contains(".sf-notification")).toBe(true);
-  });
-  it("renders the message when passed via props", () => {
-    const message = 'Hello World';
-    const component = shallowMount(SfNotification, {
-      propsData: {
-        message
-      }
-    });
-    expect(component.find(".sf-notification__text").text()).toMatch(message);
+describe("ComponentNameCamelCase.vue", () => {
+  it("renders a component", () => {
+    const component = shallowMount(ComponentNameCamelCase);
+    expect(component.contains(".ComponentNameKebabCase")).toBe(true);
   });
 });
