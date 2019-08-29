69f53aca5e94c885e21e3dd6eb3b5b4359f777aa
Adam Pawliński <adam.pawlinski@gmail.com>
2019-08-29 22:52:29 +0200

    vue and html file added, message and icon props as well

--- packages/vue/src/components/molecules/SfNotification/SfNotification.js
+++ packages/vue/src/components/molecules/SfNotification/SfNotification.js
@@ -1,3 +1,16 @@
 export default {
-  name: "SfNotification"
+  name: "SfNotification",
+  components: {
+    SfIcon
+  },
+  props: {
+    message: {
+      type: String,
+      default: ""
+    },
+    icon: {
+      type: String,
+      default: "added_to_cart"
+    }
+  }
 };
